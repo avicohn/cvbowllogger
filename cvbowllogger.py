5448babe5108e95b2d223b929265fd440f4e4d3a
avicohn <avicohn@gmail.com>
2014-07-09 17:48:30 -0700

    init commit
    created repo

---
+++ cvbowllogger.py
@@ -0,0 +1,16 @@
+"""
+computer vision bowling score logger (hope we can think of a better name)
+
+avi cohn july 2014
+
+TODO:
+
+-read in bowling jpeg
+-identify total score frame
+-write score to csv with date from jpeg
+-port to iOS (swift) and maybe android?
+"""
+
+#import opencv
+#import numpy
+
